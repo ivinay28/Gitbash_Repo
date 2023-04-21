e5042a1e8688bbf9ccad846a639ef2ebd4e64226
ivinay28 <vinay.roopangudi12@gmail.com>
2023-04-21 20:06:27 +0530

    commited

---
+++ src/main/java/TestNg/OrderOfExecution.java
@@ -0,0 +1,24 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class OrderOfExecution 
+{
+	//1st push
+	//1st pull
+	@Test(priority = 1)
+	public void createcontact()
+	{
+		System.out.println("create contact");
+	}
+	@Test(priority = 2)
+	public void modifycontact()
+	{
+		System.out.println("modify contact");
+	}
+	@Test(priority = 3)
+	public void deletecontact()
+	{
+		System.out.println("delete contact");
+	}
+}
